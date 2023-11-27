21d0c795e76e926a7d6308b52573d9b6fdd8ab78
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-11-27 17:05:55 +0000

    style: add lighter background for design area

--- app/routes/tailwind.employee-card.tsx
+++ app/routes/tailwind.employee-card.tsx
@@ -20,13 +20,13 @@ const BlockGallery = () => {
           combination of JSX elements like <code>{"<div>"}</code>,{" "}
           <code>{"<h2>"}</code> <code>{"<p>"}</code>, and Tailwind classes?
         </p>
-        <div className="bg-gray-600 mb-8 p-8 rounded border border-gray-400 max-w-xl mx-auto shadow-2xl">
+        <div className="bg-gray-400 mb-8 p-8 rounded border border-gray-400 max-w-xl mx-auto shadow-2xl">
           <img src={employeeCard} alt="Employee Card" className="w-full" />
         </div>
         <p className="text-white mb-4 text-xl max-w-2xl my-auto text-center">
           🔽 Your card will appear below... 🔽
         </p>
-        <div className="bg-gray-600 p-8 rounded border border-gray-400 max-w-xl mb-4 mx-auto shadow-2xl">
+        <div className="bg-gray-400 p-8 rounded border border-gray-400 max-w-xl mb-4 mx-auto shadow-2xl">
           <EmployeeCard />
         </div>
       </div>
